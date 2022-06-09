82a03ecbfda29f6bc0ac051d7a9d5e779fba15fb
gr546max <gr546max@yandex.ru>
2022-06-09 22:13:55 +0300

    Stage 6 end, all test good!

--- tests/_bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-
-new yii\web\Application(require(__DIR__ . '/../config/test.php'));
